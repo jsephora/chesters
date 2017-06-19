465c444036b1a4c89f731b174e8a81692ab301b1
Edmund <joeysephora@gmail.com>
2017-06-19 21:15:27 +1000

    dropdown menu bug fix

--- js/freelancer.js
+++ js/freelancer.js
@@ -29,9 +29,9 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a.nonDD').click(function() {
     $('.navbar-toggle:visible').click();
 });
